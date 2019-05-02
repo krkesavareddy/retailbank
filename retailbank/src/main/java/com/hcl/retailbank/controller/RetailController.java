93b7d17a4f748e6c9f30f9f9294ec9e5c9b565c9
akashaman29 <36605898+akashaman29@users.noreply.github.com>
2019-05-02 17:30:47 +0530

    add

--- retailbank/src/main/java/com/hcl/retailbank/controller/RetailController.java
+++ retailbank/src/main/java/com/hcl/retailbank/controller/RetailController.java
@@ -1,5 +1,6 @@
 package com.hcl.retailbank.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ public class RetailController {
 	@PostMapping("/customerregistration")
 	public ResponseEntity<String> customerRegistration(@RequestBody CustomerCreation customer){
 		customerServiceImpl.customerRegistration(customer);
+		new ResponseEntity<String>("Customer Registered successfully",HttpStatus.OK);
 	}
 
 }
